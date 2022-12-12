78b780a673d8d4f42f1cd76ef5007d4c61753138
Noam Lushi <noamlushi@gmail.com>
2022-12-12 23:59:49 +0200

    cart details comp part 2

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -3,13 +3,12 @@ const CartItem = (props) => {
   return (
     <div>
       <div className="containerCartItem">
-        <img
-          className="product-image-cart"
-          alt=""
-          src={props.imgUrl}
-        ></img>
+        <img className="product-image-cart" alt="" src={props.imgUrl}></img>
         <div className="product-details-cart">
           <h1>{props.name}</h1>
+        </div>
+
+        <div className="product-details-cart">
           <h1 className="expense-item__price">{props.price + "₪"}</h1>
         </div>
       </div>
--- src/components/cart/DetailsCart.js
+++ src/components/cart/DetailsCart.js
@@ -1,25 +1,30 @@
 import "./DetailsCart.css";
 
 const DetailsCart = (props) => {
-  const firstN = "first name";
+  const firstN = "First Name";
+  const lastN = "Last Name";
+  const phone = "Phone Number";
 
   return (
     <div>
       <div className="containerCartDetails">
+        <h1>Please Enter Your Details</h1>
         <form>
           <div class="question">
-            <input type="text" value={firstN} required />
+            <input type="text" defaultValue={firstN} required />
             <label></label>
           </div>
           <div class="question">
-            <input type="text" required />
-            <label>Last Name</label>
+            <input type="text" defaultValue={lastN} required />
           </div>
           <div class="question">
-            <input type="text" required />
-            <label>Email Address</label>
+            <input type="text" defaultValue={phone} required />
           </div>
         </form>
+        <h1>total price: {} ₪</h1>
+        <button className="btn">
+          <span className="buy">Buy Now</span>
+        </button>
       </div>
     </div>
   );
