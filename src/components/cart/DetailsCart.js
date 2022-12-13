4ae38391a9c2b0679d51d32130d992cad92219d7
HadarBasson1 <hadarbsn@gmail.com>
2022-12-13 17:09:55 +0200

    form add

--- src/components/cart/DetailsCart.js
+++ src/components/cart/DetailsCart.js
@@ -1,9 +1,45 @@
 import "./DetailsCart.css";
+import React, { useState } from 'react';
 
 const DetailsCart = (props) => {
   const firstN = "First Name";
   const lastN = "Last Name";
   const phone = "Phone Number";
+
+  const [enteredFirstN, setEnteredFirstN] = useState('');
+  const [enteredLastN, setEnteredLastN] = useState('');
+  const [enteredPhone, setEnteredPhone] = useState('');
+
+
+
+
+  const firstNChangeHandler = (event) => {
+    setEnteredFirstN(event.target.value);
+  }
+
+  const lastNChangeHandler = (event) => {
+    setEnteredLastN(event.target.value);
+  }
+
+  const phoneChangeHandler = (event) => {
+    setEnteredPhone(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const savedData = {
+      firstN: enteredFirstN,
+      lastN: enteredLastN,
+      phone: enteredPhone,
+    };
+
+    // props.onSaveExpenseData(expenseData);
+    setEnteredFirstN('');
+    setEnteredLastN('');
+    setEnteredPhone('');
+  }
+
 
   return (
     <div>
@@ -11,18 +47,22 @@ const DetailsCart = (props) => {
         <h1>Please Enter Your Details</h1>
         <form>
           <div class="question">
-            <input type="text" defaultValue={firstN} required />
-            <label></label>
+            <label>First Name</label>
+            <input type="text" defaultValue={enteredFirstN} required />
           </div>
           <div class="question">
-            <input type="text" defaultValue={lastN} required />
+            <br></br>
+            <label>Last Name</label>
+            <input type="text" defaultValue={enteredLastN} required />
           </div>
           <div class="question">
-            <input type="text" defaultValue={phone} required />
+          <br></br>
+            <label>Phon Number</label>
+            <input type="text" defaultValue={enteredPhone} required />
           </div>
         </form>
         <h1>total price: {} ₪</h1>
-        <button className="btn">
+        <button type="submit" onClick={submitHandler} className="btn">
           <span className="buy">Buy Now</span>
         </button>
       </div>
